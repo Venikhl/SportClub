296e0c6fd9804ef9be7b95a604d056722d40bd8b
Gleb <asd>
2021-10-28 17:30:25 +0600

    main page is ready

--- SportClub/resources/views/people/show.blade.php
+++ SportClub/resources/views/people/show.blade.php
@@ -1,10 +1,68 @@
 <x-layouts.navbar>
     <link rel="stylesheet" href="{{ asset("css/style_people.css") }}">
+    <div style="text-align: center; padding: 10px; border-radius: 5px; border: 4px double white; color: white; margin-top: 2%; margin-bottom: 2%; margin-left: 20%; margin-right: 20%">
+        <h1>Наши игроки!</h1>
+    </div>
 <ol class="bullet">
-    <li>Элемент списка</li>
-    <li>Элемент списка</li>
-    <li>Элемент списка</li>
-    <li>Элемент списка</li>
-    <li>Элемент списка</li>
+    <li>
+        <div class="row align-items-center">
+            <div class="col-3">
+                <img src="https://st3.depositphotos.com/19428878/35328/v/450/depositphotos_353285658-stock-illustration-unknown-male-silhouette-profile-avatar.jpg" style="max-height: 120px;">
+            </div>
+            <div class="col-5">
+                <h4>Игорь Зиновьев</h4>
+                <p>Один из наших лучших нападающих...</p>
+                <button type="button" class="btn btn-outline-light">Подробнее</button>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="row align-items-center">
+            <div class="col-3">
+                <img src="https://st3.depositphotos.com/19428878/35328/v/450/depositphotos_353285658-stock-illustration-unknown-male-silhouette-profile-avatar.jpg" style="max-height: 120px;">
+            </div>
+            <div class="col-5">
+                <h4>Михаил Агапов</h4>
+                <p>Умнейший связущий...</p>
+                <button type="button" class="btn btn-outline-light">Подробнее</button>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="row align-items-center">
+            <div class="col-3">
+                <img src="https://st3.depositphotos.com/19428878/35328/v/450/depositphotos_353285658-stock-illustration-unknown-male-silhouette-profile-avatar.jpg" style="max-height: 120px;">
+            </div>
+            <div class="col-5">
+                <h4>Антон Лищевский</h4>
+                <p>Это наш либеро...</p>
+                <button type="button" class="btn btn-outline-light">Подробнее</button>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="row align-items-center">
+            <div class="col-3">
+                <img src="https://st3.depositphotos.com/19428878/35328/v/450/depositphotos_353285658-stock-illustration-unknown-male-silhouette-profile-avatar.jpg" style="max-height: 120px;">
+            </div>
+            <div class="col-5">
+                <h4>Виктор Шишкин</h4>
+                <p>Представляем вам гениального аса команды...</p>
+                <button type="button" class="btn btn-outline-light">Подробнее</button>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="row align-items-center">
+            <div class="col-3">
+                <img src="https://st3.depositphotos.com/19428878/35328/v/450/depositphotos_353285658-stock-illustration-unknown-male-silhouette-profile-avatar.jpg" style="max-height: 120px;">
+            </div>
+            <div class="col-5">
+                <h4>Александр Копылов</h4>
+                <p>Хоть это и новичек, но жару задать может...</p>
+                <button type="button" class="btn btn-outline-light">Подробнее</button>
+            </div>
+        </div>
+    </li>
 </ol>
 </x-layouts.navbar>
